50cd9d3668f3ce9def750287668414d0682dcda2
RKostochko <kostochko.tech@mail.ru>
2021-08-19 10:03:02 +0300

    fead: comlpete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	if (!array || !array.length) {
+		return 0;
+	} else {
+		return Math.min.apply(Math, array);
+	}
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+	if (!array || !array.length) {
+		return 0;
+	} else {
+		return Math.max.apply(Math, array);
+	}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	let sum = 0;
+	if (!array || !array.length) {
+		return 0;
+	} else {
+		for (let i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		return sum / array.length;
+	}
 }
